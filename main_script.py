fdf97b2419b51ec7045252d251f3cd18af8f5068
Cyril Welschen <cj.welschen@gmail.com>
2017-07-22 16:11:23 +0200

    First pages with using an image

--- main_script.py
+++ main_script.py
@@ -2,10 +2,27 @@
 
 
 from flask import Flask
+from flask import request
+from flask import render_template
 
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
     return "Hello from the other side"
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        return "POST site"
+    else:
+        return "GET Site"
+
+
+@app.route('/hello')
+def hello(name=None):
+    name = "Cyril"
+    return render_template('hello.html', name=name)
